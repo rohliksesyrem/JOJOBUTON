913030ed1870dd3b5dc9d3b2878df90c93f60cf6
jiri.rzyman <jiri.rzyman@infis.cz>
2026-03-19 10:29:36 +0100

    asdasdasd

--- WinFormsApp4/Form1.Designer.cs
+++ WinFormsApp4/Form1.Designer.cs
@@ -32,7 +32,10 @@ partial class Form1
         button1 = new System.Windows.Forms.Button();
         button2 = new System.Windows.Forms.Button();
         trackBar1 = new System.Windows.Forms.TrackBar();
+        button3 = new System.Windows.Forms.Button();
+        trackBar2 = new System.Windows.Forms.TrackBar();
         ((System.ComponentModel.ISupportInitialize)trackBar1).BeginInit();
+        ((System.ComponentModel.ISupportInitialize)trackBar2).BeginInit();
         SuspendLayout();
         // 
         // button1
@@ -43,6 +46,7 @@ partial class Form1
         button1.TabIndex = 0;
         button1.Text = "button1";
         button1.UseVisualStyleBackColor = true;
+        button1.Click += button1_Click;
         // 
         // button2
         // 
@@ -56,24 +60,50 @@ partial class Form1
         // 
         // trackBar1
         // 
-        trackBar1.Location = new System.Drawing.Point(315, 381);
+        trackBar1.Location = new System.Drawing.Point(240, 384);
         trackBar1.Name = "trackBar1";
         trackBar1.Size = new System.Drawing.Size(279, 45);
         trackBar1.TabIndex = 2;
+        trackBar1.Scroll += trackBar1_Scroll;
+        // 
+        // button3
+        // 
+        button3.Location = new System.Drawing.Point(260, 117);
+        button3.Name = "button3";
+        button3.Size = new System.Drawing.Size(198, 83);
+        button3.TabIndex = 3;
+        button3.Text = "button3";
+        button3.UseVisualStyleBackColor = true;
+        button3.Click += button3_Click;
+        // 
+        // trackBar2
+        // 
+        trackBar2.Location = new System.Drawing.Point(42, 38);
+        trackBar2.Name = "trackBar2";
+        trackBar2.Size = new System.Drawing.Size(207, 45);
+        trackBar2.TabIndex = 4;
+        trackBar2.Scroll += trackBar2_Scroll;
         // 
         // Form1
         // 
         AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
         AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
         ClientSize = new System.Drawing.Size(800, 450);
+        Controls.Add(trackBar2);
+        Controls.Add(button3);
         Controls.Add(trackBar1);
         Controls.Add(button2);
         Controls.Add(button1);
         Text = "Form1";
         ((System.ComponentModel.ISupportInitialize)trackBar1).EndInit();
+        ((System.ComponentModel.ISupportInitialize)trackBar2).EndInit();
         ResumeLayout(false);
         PerformLayout();
     }
+
+    private System.Windows.Forms.TrackBar trackBar2;
+
+    private System.Windows.Forms.Button button3;
 
     private System.Windows.Forms.Button button1;
     private System.Windows.Forms.Button button2;
--- WinFormsApp4/Form1.cs
+++ WinFormsApp4/Form1.cs
@@ -3,18 +3,43 @@ using System.Windows.Media;
 namespace WinFormsApp4;
 
 public partial class Form1 : Form
-{
+{  MediaPlayer jojo = new MediaPlayer();
+
     public Form1()
     {
-        MediaPlayer jojo = new MediaPlayer();
-
-        jojo.Open(new Uri("rero-rero-rero.mp3",UriKind.Relative));
-        jojo.Play();
         InitializeComponent();
     }
 
     private void button2_Click(object sender, EventArgs e)
     {
-        MediaPlayer jojo = new MediaPlayer();
+       
+        jojo.Open(new Uri("rero-rero-rero.mp3",UriKind.Relative));
+        jojo.Play();
+      
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+       
+        jojo.Open(new Uri("-jojo-nigerundayo.mp3", UriKind.Relative));
+        jojo.Play();
+      
+    }
+
+    private void button3_Click(object sender, EventArgs e)
+    {
+        jojo.Open(new Uri("doppio-ringing.mp3", UriKind.Relative));
+        jojo.Play();
+       
+    }
+
+    private void trackBar1_Scroll(object sender, EventArgs e)
+    {
+        jojo.Volume = trackBar1.Value / 100.0;
+    }
+
+    private void trackBar2_Scroll(object sender, EventArgs e)
+    {
+        jojo.SpeedRatio = trackBar2.Value / 100.0;
     }
 }
